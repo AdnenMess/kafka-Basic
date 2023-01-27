2877bd24b54083c4e73b66ad53835da80db20e3d
AdnenMess <adnen_messaoudi@hotmail.com>
2023-01-27 18:16:57 +0100

    update producer

--- code/producer.py
+++ code/producer.py
@@ -1,5 +1,5 @@
-
 from kafka import KafkaProducer
+# pip install kafka-python
 
 # Create Message
 msg = 'Hello this is a test message'
@@ -7,25 +7,27 @@ msg = 'Hello this is a test message'
 # Create a producer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
+
 # Function to send messages into Kafka
 def kafka_python_producer_async(producer, msg):
-    
     producer.send('mytesttopic', msg).add_callback(success).add_errback(error)
     producer.flush()
+
 
 def success(metadata):
     print(metadata.topic)
 
+
 def error(exception):
-    print(exception)  
+    print(exception)
+
 
 print("start producing")
 
 # Produce the message, but serialize into bytes
-kafka_python_producer_async(producer,bytes(msg, 'utf-8'))
+kafka_python_producer_async(producer, bytes(msg, 'utf-8'))
 
 print("done")
- 
 
 '''
 def kafka_python_producer_sync(producer, size):
